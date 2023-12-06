a304067be685cad456eb3d038bd225afcb3fbb50
GrPugaguga <pugaguga@mail.ru>
2023-12-06 23:16:51 +0500

    #12 Поправил пробелы

--- lesson 5/index.js
+++ lesson 5/index.js
@@ -8,6 +8,7 @@ const variantsButton = {
     users: 'users',
 };
 const data = {
+    BASE_URL: 'https://jsonplaceholder.typicode.com/',
     limit: 10,
     page: 1,
     count: 0
@@ -19,60 +20,62 @@ const pageBtnContainer = document.createElement('div')
 pageBtnContainer.classList.add('pageBtnContainer')
 
 
-const getData = async function (url) {
+const getData = async function (props, variants) {
     try {
-        let response = await fetch(`https://jsonplaceholder.typicode.com/${url ? url : 'posts'}?_limit=${data.limit}&_page=${data.page}`, { headers: variantsButton.headers });
+        console.log(`${props.BASE_URL}${variants ? variants : 'posts'}?_limit=${props.limit}&_page=${props.page}`)
+        let response = await fetch(`${props.BASE_URL}${variants ? variants : 'posts'}?_limit=${props.limit}&_page=${props.page}`);
         let result = await response.json();
         return result
     } catch (e) {
-        console.log(`Ошибка( ${e}`)
+        console.log(e)
     }
 }
 
-const getLength = async function (url) {
+const getLength = async function (props, variants) {
     try {
-        let response = await fetch(`https://jsonplaceholder.typicode.com/${url ? url : 'posts'}?_limit=180`);
+        let response = await fetch(`${props.BASE_URL}${variants ? variants : 'posts'}?_limit=180`);
         let result = await response.json();
         return result.length
     } catch (e) {
-        console.log(`Ошибка( ${e}`)
+        console.log(e)
     }
 }
 /*
  функция getCount сделана только потому что не смог достать значение 'X-Total-Count' из headers
  браузер его показывает в заголовке ответов, однако в response(response.headers) его нет
 */
-const getCount = async function (url) {
-    data.count = await getLength(url);
-    renderSelect(url)
+const getCount = async function (props, variants) {
+    data.count = await getLength(props, variants);
+    renderSelect(props, variants)
 }
 
-function ucFirst(str) {
+function getUppFirstEl(str) {
     if (!str) return str;
 
     return str[0].toUpperCase() + str.slice(1);
 }
 
-function renderButton() {
-    getCount('posts')
+function renderVarButton(props) {
+    console.log({ ...props })
+    getCount(props)
     for (let key in variantsButton) {
         const button = document.createElement('button');
-        button.innerHTML = `${ucFirst(key)}`;
+        button.innerHTML = `${getUppFirstEl(key)}`;
         button.classList.add('variantsButton');
         button.addEventListener('click', () => {
-            data.limit = 10;
-            data.page = 1;
-            getCount(variantsButton[key]);
-            renderCards(variantsButton[key])
+            props.limit = 10;
+            props.page = 1;
+            getCount(props, variantsButton[key]);
+            renderCards(props, variantsButton[key])
         })
         buttonContainer.appendChild(button)
     }
 }
 
-function renderSelect(url) {
+function renderSelect(props, variants) {
     pageContainer.innerHTML = ''
     pageContainer.appendChild(pageBtnContainer)
-    var selectList = document.createElement("select");
+    const selectList = document.createElement("select");
     selectList.classList.add = "selectList";
     selectList.id = 1
     for (let opt of [10, 20, 30]) {
@@ -81,47 +84,49 @@ function renderSelect(url) {
         option.text = opt;
         selectList.appendChild(option);
     }
-    renderPadination(url)
+    renderPadination(props, variants)
     selectList.addEventListener('change', (e) => {
-        data.limit = e.target.value;
-        renderPadination(url)
+        props.limit = e.target.value;
+        renderPadination(props, variants)
     })
 
     pageContainer.prepend(selectList);
 }
 
-function renderPadination(url) {
+function renderPadination(props, variants) {
     pageBtnContainer.innerHTML = ''
-    for (let i = 0; i < (data.count / data.limit); i++) {
+    for (let i = 0; i < (props.count / props.limit); i++) {
         const pageBtn = document.createElement('button');
-        pageBtn.innerHTML = `${i * data.limit + 1} - ${(i + 1) * data.limit} `
+        pageBtn.innerHTML = `${i * props.limit + 1} - ${(i + 1) * props.limit} `
         pageBtn.addEventListener('click', () => {
             data.page = i + 1
-            renderCards(url)
+            renderCards(props, variants)
         })
         pageBtnContainer.appendChild(pageBtn)
     }
 }
 
-async function renderCards(url) {
-    let cards = await getData(url);
-    cardsContainer.innerHTML = ` <h1>${ucFirst(url)}:</ h1> `
+async function renderCards(props, variants) {
+    let cards = await getData(props, variants);
+    cardsContainer.innerHTML = ` <h1>${getUppFirstEl(variants)}:</ h1> `
     for (let card of cards) {
         const cardDiv = document.createElement('div');
         for (let property in card) {
-            if (property == 'thumbnailUrl') {
-                cardDiv.innerHTML += `<img src='${card[property]}'/>`;
-                cardDiv.classList.add('cardWImg');
-            } else if (property == "company" || property == "address") {
-                if (property == "company") {
-                    cardDiv.innerHTML += `<span>${ucFirst(property)}:</span> ${card[property]['name']} <br/>`
-                } else {
-                    cardDiv.innerHTML += `<span>${ucFirst(property)}:</span> ${card[property]['city']}, ${card[property]["street"]}, ${card[property]["suite"]} <br/>`
-                }
-
-            } else {
-                cardDiv.innerHTML += `<span>${ucFirst(property)}:</span> ${card[property]} <br/>`
+            switch (property) {
+                case property == 'thumbnailUrl':
+                    cardDiv.innerHTML += `<img src='${card[property]}'/>`;
+                    cardDiv.classList.add('cardWImg');
+                    break;
+                case property == "company":
+                    cardDiv.innerHTML += `<span>${getUppFirstEl(property)}:</span> ${card[property]['name']} <br/>`
+                    break;
+                case property == "address":
+                    cardDiv.innerHTML += `<span>${getUppFirstEl(property)}:</span> ${card[property]['city']}, ${card[property]["street"]}, ${card[property]["suite"]} <br/>`
+                    break;
+                default:
+                    cardDiv.innerHTML += `<span>${getUppFirstEl(property)}:</span> ${card[property]} <br/>`
             }
+            /* Ранее никогда не использовал, но кажется в данной ситуации приятнее чем if else*/
         }
         cardDiv.innerHTML += `<hr/>`
         cardDiv.classList.add('cardDiv');
@@ -129,5 +134,10 @@ async function renderCards(url) {
     }
 }
 
-renderButton()
-renderCards('posts')
+function fullRenderMainView(props) {
+    renderVarButton(props)
+    renderCards(props, 'posts')
+}
+
+fullRenderMainView(data)
+
